4ca0feca410b264984525aeff5ee0d1c9a0c01a7
plant0303 <minheee07@gmail.com>
2024-11-11 17:31:08 +0900

    a1111

---
+++ vsJava/java/src/a1111/Var1.java
@@ -0,0 +1,9 @@
+package a1111;
+
+public class Var1 {
+    public static void main(String[] args) {
+        int value = 1;
+        int result = value + 10;
+        System.out.println(result);
+    }
+}
---
+++ vsJava/java/src/a1111/Var2.java
@@ -0,0 +1,12 @@
+package a1111;
+
+public class Var2 {
+    public static void main(String[] args) {
+        int hour = 3;
+        int minute = 5;
+        int totalMinute = 0;
+        System.out.println(hour + "시간" + minute + "분");
+        System.out.println(hour * 60 + minute + "분");
+        
+    }
+}
